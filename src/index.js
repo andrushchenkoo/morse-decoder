6139d876df8e2e484db4798dd2544391eebfdad5
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-09-01 15:28:47 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,44 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let result = '';
+  const chunksLength = 10;
+  const chunks = [];
+  const numberChunks = [];
+
+  for (let i = 0; i < expr.length / chunksLength; i += 1) {
+    const chunk = expr.slice(i * chunksLength, (i + 1) * chunksLength);
+    chunks.push(chunk);
+  }
+
+  for (let i = 0; i < chunks.length; i += 1) {
+    if (chunks[i] === '**********') {
+      numberChunks.push(' ');
+    } else {
+      let morseChunk = '';
+
+      for (let j = 0; j < chunksLength; j += 2) {
+        const pair = chunks[i][j] + chunks[i][j + 1];
+
+        if (pair === '10') {
+          morseChunk += '.';
+        } else if (pair === '11') {
+          morseChunk += '-';
+        }
+      }
+
+      numberChunks.push(morseChunk);
+    }
+  }
+  numberChunks.forEach((chunk) => {
+    if (chunk === ' ') {
+      result += chunk;
+    } else {
+      result += MORSE_TABLE[chunk];
+    }
+  });
+
+  return result;
 };
